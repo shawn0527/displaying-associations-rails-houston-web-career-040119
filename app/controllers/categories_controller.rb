8666898096a47fedb0545fab05acc6594d02c518
Shawn Ren <shawnren527@gmail.com>
2019-04-29 19:42:56 +0000

    Automatically backed up by Learn

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -25,4 +25,8 @@ class CategoriesController < ApplicationController
     category.update(params.require(:category))
     redirect_to category_path(category)
   end
+
+  private
+  def category_params
+    params.require(:category).permit(:name)
 end
